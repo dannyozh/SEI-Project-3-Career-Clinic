315abbe628d6ce907e4cf37421feb2f14f69d69d
dannyozh <danielongzhihao@gmail.com>
2019-11-11 15:39:38 +0800

    added static landing page and landing controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :traits
   resources :employers
   resources :listings
-  root "explorers#index"
   devise_for :explorers
+  root "landings#landing"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
