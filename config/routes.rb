ed7849613bd7fb7c01d1c6f2996fcd30ab559d01
dannyozh <danielongzhihao@gmail.com>
2019-11-11 14:43:19 +0800

    created devise users table, model, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "explorers#index"
+  devise_for :explorers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
