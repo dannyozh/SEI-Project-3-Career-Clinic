4482028c019699b3c39ee0038fb2e1b1f567022f
dannyozh <danielongzhihao@gmail.com>
2019-11-12 23:40:54 +0800

    revised migration table for industries and listings as one reference column was wrong. Please pull origin master, drop db, create db, migrate and seed all over again.

--- db/migrate/20191112060746_industries_listings.rb
+++ db/migrate/20191112060746_industries_listings.rb
@@ -2,7 +2,7 @@ class IndustriesListings < ActiveRecord::Migration[5.2]
   def change
     create_table :industries_listings do |t|
       t.references :listing
-      t.references :trait
+      t.references :industry
       t.timestamps
     end
   end
