4624c5b2451a1bec4b785334a5a19a2572c510e1
dannyozh <danielongzhihao@gmail.com>
2019-11-17 17:25:26 +0800

    add 'connect' button on employer side. Explorer side will see connect status on their side

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -65,14 +65,27 @@ class ListingsController < ApplicationController
       @employers_profile = EmployersProfile.find_by(:employer_id => current_employer.id)
       @employersListing = ExplorersProfilesListing.where(:listing_id => params[:id]).map { |x| x.listing_id }
       @explorer_profile_ids = Listing.find_by(:employer_profile_id => current_employer.id).explorers_profiles_listing.map { |x| x.explorers_profile_id }
+
       #go to listing table and find listing which belongs to this employer and find the explorer that has interest in this listing in the inner join table
       @profiles = ExplorersProfile.where("id IN (?)", @explorer_profile_ids)
-      @profiles.each do |applicant|
-        applicant.id
-        p "@@@@@@@@@@@@@ id", applicant.id
-      end
+      # p "&&&&&&&&&&", @profiles
     elsif current_explorer
       @explorers_profile = ExplorersProfile.find_by(:explorer_id => current_explorer.id)
+      # p "@@@@@@@@@@", @listing.id
+      @queryStatus = ExplorersProfilesListing.where(:explorers_profile_id => current_explorer.id, :listing_id => @listing.id, :connect => "true")
+    end
+  end
+
+  def link
+    @listing = Listing.find(params[:listid])
+    @explorers_profile = ExplorersProfile.find(params[:exid])
+    # # @profiles = ExplorersProfile.where("id IN (?)", @explorer_profile_ids)
+    @findListing = ExplorersProfilesListing.where(:explorers_profile_id => @explorers_profile.id, :listing_id => @listing.id, :connect => "true")
+    if @findListing.exists?
+      redirect_to @listing, danger: "You're already in touch!"
+    else
+      ExplorersProfilesListing.where(:explorers_profile_id => @explorers_profile.id, :listing_id => @listing.id).update(connect: "true")
+      redirect_to @listing, success: "You're connected!"
     end
   end
 
@@ -155,13 +168,13 @@ class ListingsController < ApplicationController
 
   def interest
     @listing = Listing.find(params[:listid])
-    p "%%%%%%%%%%%%", @listing.id
+    # p "%%%%%%%%%%%%", @listing.id
     @explorers_profile = ExplorersProfile.find(params[:exid])
-    p "@@@@@@@@@@@", @explorers_profile.id
+    # p "@@@@@@@@@@@", @explorers_profile.id
     @interestListing = ExplorersProfilesListing.new(:explorers_profile_id => @explorers_profile.id, :listing_id => @listing.id)
-    p "$$$$$$$$$", @interestListing
+    # p "$$$$$$$$$", @interestListing
     @findListing = ExplorersProfilesListing.where(:explorers_profile_id => @explorers_profile.id, :listing_id => @listing.id)
-    p "&&&&&&&&&&&&*&*&*", @findListing
+    # p "&&&&&&&&&&&&*&*&*", @findListing
     if @findListing.exists?
       redirect_to @listing, danger: "You've already registered interest"
     else
@@ -170,17 +183,17 @@ class ListingsController < ApplicationController
     end
   end
 
-  def connect
-    @explorer_profile_ids = Listing.find_by(:employer_profile_id => current_employer.id).explorers_profiles_listing.map { |x| x.explorers_profile_id }
-    #go to listing table and find listing which belongs to this employer and find the explorer that has interest in this listing in the inner join table
-    @profiles = ExplorersProfile.where("id IN (?)", @explorer_profile_ids)
-    #find the explorer's profile
-    if current_employer
-      @employers_profile = EmployersProfile.find_by(:employer_id => current_employer.id)
-    elsif current_explorer
-      @explorers_profile = ExplorersProfile.find_by(:explorer_id => current_explorer.id)
-    end
-  end
+  # def connect
+  #   @explorer_profile_ids = Listing.find_by(:employer_profile_id => current_employer.id).explorers_profiles_listing.map { |x| x.explorers_profile_id }
+  #   #go to listing table and find listing which belongs to this employer and find the explorer that has interest in this listing in the inner join table
+  #   @profiles = ExplorersProfile.where("id IN (?)", @explorer_profile_ids)
+  #   #find the explorer's profile
+  #   if current_employer
+  #     @employers_profile = EmployersProfile.find_by(:employer_id => current_employer.id)
+  #   elsif current_explorer
+  #     @explorers_profile = ExplorersProfile.find_by(:explorer_id => current_explorer.id)
+  #   end
+  # end
 end
 
 private
